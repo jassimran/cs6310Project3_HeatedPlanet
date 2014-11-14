31aefcbb68c4f2ee25960843d9fa86ccecdcb948
Brandon <atobrandon@gmail.com>
2014-11-14 16:58:27 -0600

    Working on computing the query results

--- HeatedPlanet/src/controllers/QueryControl.java
+++ HeatedPlanet/src/controllers/QueryControl.java
@@ -10,14 +10,19 @@ import domain.EarthGrid;
 import domain.Simulation;
 
 public class QueryControl {
-
+	
+	private static PersistenceService persistenceService;
+	
+	public QueryControl(){
+		persistenceService = PersistenceService.getInstance();
+	}
+	
 	public boolean simulationNameExists(String simulationName) {
 		return findSimulationByName(simulationName) != null;
 	}
 
 	public Simulation findSimulationByName(String simulationName) {
-		List<Simulation> simulations = PersistenceService.getInstance()
-				.searchSimulations(simulationName);
+		List<Simulation> simulations = persistenceService.searchSimulations(simulationName);
 		if (simulations.isEmpty())
 			return null;
 		else
@@ -39,15 +44,46 @@ public class QueryControl {
 					.getSimulatedDate().after(startDate))
 					&& (timeStep.getSimulatedDate().equals(endDate) || timeStep
 							.getSimulatedDate().before(endDate))) {
-				
+
 				List<EarthCell> matchingCells = new ArrayList<EarthCell>();
-				
-				for(EarthCell currentCell : timeStep.getNodeList()){
-					//if(currentCell.)
+
+				// if all lat/long params are 0 use the whole planet
+				if (startLat == 0 && endLat == 0 && startLong == 0 && endLong == 0)
+					matchingCells = timeStep.getNodeList();
+				else {
+					for (EarthCell currentCell : timeStep.getNodeList()) {
+						double cellLat = 0; // TODO get the latitude for the
+											// current cell
+						double cellLong = 0; // TODO get the longitude for the
+											 // current cell
+
+						boolean latMatch = false;
+						if (startLat > 0 && endLat < 0) {
+							if ((cellLat >= startLat && cellLat <= 180)
+									|| cellLat >= -180 && cellLat <= endLat) {
+								latMatch = true;
+							}
+						} else if (cellLat >= startLat && cellLat <= endLat) {
+							latMatch = true;
+						}
+						
+						boolean longMatch = false;
+						if (startLong > 0 && endLong < 0) {
+							if ((cellLong >= startLong && cellLong <= 90)
+									|| cellLong >= -90 && cellLong <= endLong) {
+								longMatch = true;
+							}
+						} else if (cellLong >= startLong && cellLong <= endLong) {
+							longMatch = true;
+						}
+						
+						if(latMatch && longMatch)
+							matchingCells.add(currentCell);
+					}
 				}
-				
+
 				timeStep.setNodeList(matchingCells);
-				
+
 				matchingGrids.add(timeStep);
 			}
 		}
