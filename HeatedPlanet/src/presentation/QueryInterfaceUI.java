50e71cefbd26f8ca20e4ce8d1119f44f86d1bfae
sm3 <subha.mela@gatech.edu>
2014-11-16 12:36:49 -0800

    aligned comments

--- HeatedPlanet/src/presentation/QueryInterfaceUI.java
+++ HeatedPlanet/src/presentation/QueryInterfaceUI.java
@@ -514,9 +514,7 @@ cell:");
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code 
-
-(optional) ">
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the 
 
 default look and feel.
