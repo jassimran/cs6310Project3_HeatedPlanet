025a691b87bff3ae805a38bf1741c50720064ed6
Jaime Soto <jsoto.gt@gmail.com>
2014-11-20 22:52:20 -0500

    made abastract control factory singleton

--- HeatedPlanet/src/controllers/AbstractControlFactory.java
+++ HeatedPlanet/src/controllers/AbstractControlFactory.java
@@ -1,10 +1,25 @@
 package controllers;
 
+
 /**
  * Singleton object used to create instances of AbastractControl objects.
  * @author jsoto
  *
  */
 public class AbstractControlFactory {
+	
+	// singleton instance
+	private static AbstractControlFactory abstractControlFactory;
+		
+	private AbstractControlFactory() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static synchronized AbstractControlFactory getInstance() {
+		if(abstractControlFactory == null) {
+			abstractControlFactory = new AbstractControlFactory();
+		}
+		return abstractControlFactory;
+	}
 
 }
