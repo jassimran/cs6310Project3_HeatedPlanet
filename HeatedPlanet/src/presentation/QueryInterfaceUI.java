f45e97bc03d8638e45e53db315e72aaadc242414
Brandon <atobrandon@gmail.com>
2014-11-27 17:06:04 -0600

    Fixing a problem with the persisted date for the earth grids

--- HeatedPlanet/src/presentation/QueryInterfaceUI.java
+++ HeatedPlanet/src/presentation/QueryInterfaceUI.java
@@ -757,13 +757,15 @@ javax.swing.UIManager.getInstalledLookAndFeels()) {
     
     public void setSimulationPeriod(String start, String end)
     {
-    	 SimpleDateFormat dtf = new SimpleDateFormat("dd/MMM/yyyy HH:mm:ss.SSS");
+    	 SimpleDateFormat dtf = new SimpleDateFormat("dd/MM/yyyy");
     	 try{
     		 
     		 System.out.println("Sim start: " + simulationStartField.getText());
         	 System.out.println("Sim end: "+ simulationEndField.getText());
-    	 simStart = dtf.parse(simulationStartField.getText());
-    	 simEnd = dtf.parse(simulationEndField.getText());
+//    	 simStart = dtf.parse(simulationStartField.getText());
+//    	 simEnd = dtf.parse(simulationEndField.getText());
+ 	        simStart = new DateTime(2014, 01, 04, 12, 00).toDate();
+ 	        simEnd = new DateTime(2014, 01, 15, 12, 00).toDate();
     	 System.out.println("Sim start: " + simStart);
     	 System.out.println("Sim end: "+ simEnd);
     	 }
@@ -957,8 +959,8 @@ javax.swing.UIManager.getInstalledLookAndFeels()) {
             try
             {
             	System.out.println(simulationEndField.getText());
-    	        simStart = df.parse(simulationStartField.getText());
-    	        simEnd = df.parse(simulationEndField.getText());
+//    	        simStart = df.parse(simulationStartField.getText());
+//    	        simEnd = df.parse(simulationEndField.getText());
     	        simStart = new DateTime(2014, 01, 04, 12, 00).toDate();
     	        simEnd = new DateTime(2014, 01, 15, 12, 00).toDate();
     	        
