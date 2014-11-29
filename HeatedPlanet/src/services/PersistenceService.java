65015a8536f722b389fdc469dbe4d10a210b9877
Jaime Soto <jsoto.gt@gmail.com>
2014-11-29 08:55:02 -0500

    Removed warnings

--- HeatedPlanet/src/services/PersistenceService.java
+++ HeatedPlanet/src/services/PersistenceService.java
@@ -1,18 +1,12 @@
 package services;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.ParameterExpression;
-import javax.persistence.criteria.Root;
 
 import persistence.EntityManagerFactory;
 import presentation.earth.TemperatureGrid;
